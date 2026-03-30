b5eabc88e55bf54caa4e85dea3d2ed72e57c10f4
Natanael Wijaya <nw@Mac.lan>
2026-03-29 21:37:07 -0400

    Added PWA Service Worker

---
+++ frontend/public/service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'cookmark-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/js/main.chunk.js',
+  '/static/js/bundle.js',
+  '/static/css/main.chunk.css',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
